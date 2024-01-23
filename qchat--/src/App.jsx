aba7dc4380d57b0cb0c5e2fe8df233d657e60ac3
bakomans <bakomans@gmail.com>
2024-01-23 07:25:04 +0000

    added buttons for login and reg

--- qchat--/src/App.jsx
+++ qchat--/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Registration from './components/Registration';
 import { AuthProvider } from './Context/AuthContext';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './components/LoginPage';
+import LoginPage from './components/loginPage';
 import Chats from './components/Chats';
 import HomePage from './components/HomePage'; // Import HomePage component
 
