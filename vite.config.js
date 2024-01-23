54d63ad691c8d920df2d41b96bdaafe8ab023472
yvonne <yvonnewong1107@gmail.com>
2024-01-23 20:01:39 +0000

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext',
+    polyfillDynamicImport: false,
+  },
   esbuild: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
+    jsxInject: `import React from 'react';`,
   },
 });
