4f06e780111cf30b216c7e2396e7ef9f13952132
yvonne <yvonnewong1107@gmail.com>
2024-01-23 20:11:23 +0000

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,31 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+  // Fix large chunks issue due to node_modules
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+    target: 'esnext', // Set the target to esnext
+    polyfillDynamicImport: false, // Disable dynamic import polyfill
+  },
+  esbuild: {
+    jsxInject: `import React from 'react';`, // Inject React import for JSX
+  },
 });
